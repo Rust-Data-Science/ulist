48debe4f23f3be5fea600e8d6d86ba127566d27a
tushushu <m18591915960@163.com>
2021-11-14 16:48:51 +0800

    expose rust methods to python.

--- ulist/src/lib.rs
+++ ulist/src/lib.rs
@@ -36,6 +36,34 @@ struct FloatList {
     list: Vec<f32>,
 }
 
+#[pymethods]
+impl FloatList {
+    #[new]
+    fn new(list: Vec<f32>) -> Self {
+        FloatList { list }
+    }
+
+    pub fn max(&self) -> f32 {
+        List::max(self)
+    }
+
+    pub fn mean(&self) -> f32 {
+        List::mean(self)
+    }
+
+    pub fn min(&self) -> f32 {
+        List::min(self)
+    }
+
+    pub fn size(&self) -> usize {
+        List::size(self)
+    }
+
+    pub fn sum(&self) -> f32 {
+        List::sum(self)
+    }
+}
+
 impl<'a> List<'a, f32> for FloatList {
     fn values(&'a self) -> &'a Vec<f32> {
         &self.list
@@ -52,14 +80,6 @@ impl<'a> List<'a, f32> for FloatList {
     }
 }
 
-#[pymethods]
-impl FloatList {
-    #[new]
-    fn new(list: Vec<f32>) -> Self {
-        FloatList { list }
-    }
-}
-
 #[pyproto]
 impl PySequenceProtocol for FloatList {
     fn __len__(&self) -> usize {
@@ -73,6 +93,34 @@ struct IntegerList {
     list: Vec<i32>,
 }
 
+#[pymethods]
+impl IntegerList {
+    #[new]
+    fn new(list: Vec<i32>) -> Self {
+        IntegerList { list }
+    }
+
+    pub fn max(&self) -> i32 {
+        List::max(self)
+    }
+
+    pub fn mean(&self) -> f32 {
+        List::mean(self)
+    }
+
+    pub fn min(&self) -> i32 {
+        List::min(self)
+    }
+
+    pub fn size(&self) -> usize {
+        List::size(self)
+    }
+
+    pub fn sum(&self) -> i32 {
+        List::sum(self)
+    }
+}
+
 impl<'a> List<'a, i32> for IntegerList {
     fn values(&'a self) -> &'a Vec<i32> {
         &self.list
@@ -84,14 +132,6 @@ impl<'a> List<'a, i32> for IntegerList {
 
     fn min(&'a self) -> i32 {
         *self.values().iter().min().unwrap()
-    }
-}
-
-#[pymethods]
-impl IntegerList {
-    #[new]
-    fn new(list: Vec<i32>) -> Self {
-        IntegerList { list }
     }
 }
 
