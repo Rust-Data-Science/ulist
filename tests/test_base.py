679fcf9949696ff9118711b02f697576b3115aa9
tushushu <m18591915960@163.com>
2021-12-11 11:51:31 +0800

    check type in test base.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,25 +1,25 @@
+from collections import abc
+from typing import List, Union
+
 import pytest
 import ulist as ul
-from typing import Union, List
 
 NUM_TYPE = Union[float, int]
 LIST_TYPE = Union[List[float], List[int]]
 
 
 @pytest.mark.parametrize(
-    "dtype, nums",
+    "dtype, nums, test_method, expected_value",
     [
-        ("float", [1, 0]),
-        ("int", [1, 0]),
-        ("bool", [True, False]),
-    ],
-)
-@pytest.mark.parametrize(
-    "test_method, expected_value",
-    [
-        ("copy", [1, 0]),
-        ("size", 2),
-        ("to_list", [1, 0]),
+        ("float", [1.0, 2.0], "copy", [1.0, 2.0]),
+        ("float", [1.0, 2.0], "size", 2),
+        ("float", [1.0, 2.0], "to_list", [1.0, 2.0]),
+        ("int", [1, 2], "copy", [1, 2]),
+        ("int", [1, 2], "size", 2),
+        ("int", [1, 2], "to_list", [1, 2]),
+        ("bool", [True, False], "copy", [True, False]),
+        ("bool", [True, False], "size", 2),
+        ("bool", [True, False], "to_list", [True, False]),
     ],
 )
 def test_methods_no_arg(
@@ -38,22 +38,23 @@ def test_methods_no_arg(
         + f" result - {result}"
         + f" expected - {expected_value}"
     )
-    assert result == expected_value, msg
+    if isinstance(result, abc.Iterable):
+        for x, y in zip(result, expected_value):
+            assert type(x) == type(y) and x == y, msg
+    else:
+        assert type(result) == type(expected_value), msg
+        assert result == expected_value, msg
 
 
 @pytest.mark.parametrize(
-    "dtype, nums",
+    "dtype, nums, test_method, expected_value, kwargs",
     [
-        ("float", [1, 0, 3]),
-        ("int", [1, 0, 3]),
-        ("bool", [True, False, True]),
-    ],
-)
-@pytest.mark.parametrize(
-    "test_method, expected_value, kwargs",
-    [
-        ("__getitem__", 0, {"index": 1}),
-        ("get", 0, {"index": 1}),
+        ("float", [1.0, 2.0, 3.0], "__getitem__", 2.0, {"index": 1}),
+        ("float", [1.0, 2.0, 3.0], "get", 2.0, {"index": 1}),
+        ("int", [1, 2, 3], "__getitem__", 1, {"index": 0}),
+        ("int", [1, 2, 3], "get", 1, {"index": 0}),
+        ("bool", [True, False, True], "__getitem__", True, {"index": 2}),
+        ("bool", [True, False, True], "get", True, {"index": 2}),
     ],
 )
 def test_methods_with_args(
@@ -73,4 +74,9 @@ def test_methods_with_args(
         + f" result - {result}"
         + f" expected - {expected_value}"
     )
-    assert result == expected_value, msg
+    if isinstance(result, abc.Iterable):
+        for x, y in zip(result, expected_value):
+            assert type(x) == type(y) and x == y, msg
+    else:
+        assert type(result) == type(expected_value), msg
+        assert result == expected_value, msg
