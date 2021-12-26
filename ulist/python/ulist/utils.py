fd42f86798667eaab597c61cd3d4ec33b221a4d9
tushushu <m18591915960@163.com>
2021-12-26 22:21:21 +0800

    add docstrings for all.

--- ulist/python/ulist/utils.py
+++ ulist/python/ulist/utils.py
@@ -12,6 +12,14 @@ def check_test_result(
     result: Union[ELEM, LIST_PY, ul.UltraFastList],
     expected_value: Union[ELEM, LIST_PY],
 ):
+    """Test if the result is as expected. Both value and type.
+
+    Args:
+        dtype (str): 'int', 'float' or 'bool'.
+        test_method (Union[Callable, str]): A method name or a function.
+        result (Union[ELEM, LIST_PY, ul.UltraFastList])
+        expected_value (Union[ELEM, LIST_PY])
+    """
     msg = (
         f"dtype - {dtype}"
         + f" test_method - {test_method}"
