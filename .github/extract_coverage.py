0b4ead144ceb0047a631d0de82b95c7d0538b702
tushushu <m18591915960@163.com>
2022-01-09 14:55:59 +0800

    fix shell command

--- .github/extract_coverage.py
+++ .github/extract_coverage.py
@@ -3,14 +3,27 @@ import subprocess
 
 
 def run() -> None:
+    # Score
     with open("../htmlcov/index.html", "r") as file:
         text = file.read()
     start = text.index("total")
     text = text[start:]
-    result = re.findall('[0-9]+\.?[0-9]+%{1}', text)[0]
-    print(f"The coverage is {result}!")
+    score = float(re.findall('[0-9]+\.?[0-9]+%{1}', text)[0].replace('%', ''))
+    print(f"The coverage is {score}%!")
+    subprocess.run([f'echo "total={score}%" >> $GITHUB_ENV'], shell=True)
 
-    subprocess.run([f'echo "total={result}" >> $GITHUB_ENV'])
+    # Color
+    if score < 70:
+        badge_color = 'red'
+    elif score < 80:
+        badge_color = 'orange'
+    elif score < 90:
+        badge_color = 'yellow'
+    else:
+        badge_color = 'green'
+    subprocess.run(
+        [f'echo "badge_color={badge_color}" >> $GITHUB_ENV'], shell=True)
+
     print("Result saved into `GITHUB_ENV`!")
 
 
