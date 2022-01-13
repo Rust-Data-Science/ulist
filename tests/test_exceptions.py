64264c5d583b8603436fcce61156f6db237cf961
tushushu <m18591915960@163.com>
2022-01-14 06:51:24 +0800

    fix ci

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,14 +4,19 @@ import pytest
 import ulist as ul
 
 _ARR = ul.from_seq(range(3), dtype='int')
-_ARR._values = [0, 1, 2]
+_ARR._values = [0, 1, 2]  # type: ignore
 
 
 @pytest.mark.parametrize(
     "test_method, kwargs, expected_error",
     [
-        (_ARR._arithmetic_method, {
-         "other": None, "fn": _ARR.add, "fn_scala": _ARR.add_scala}, TypeError),
+        (
+            _ARR._arithmetic_method,
+            {
+                "other": None, "fn": _ARR.add, "fn_scala": _ARR.add_scala
+            },
+            TypeError
+        ),
         (_ARR.astype, {"dtype": "foo"}, ValueError),
         (ul.cycle, {"obj": [1, 2], "size": 3, "dtype": "foo"}, ValueError),
         (ul.from_seq, {"obj": [1, 2], "dtype": "foo"}, ValueError),
