47ab8773e89441d20f7dd601777a3145e1664ac8
tushushu <m18591915960@163.com>
2022-01-28 22:47:37 +0800

    add test cases

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -24,22 +24,36 @@ from ulist.utils import check_test_result
         (ul.repeat, (False, 3), {}, [False, False, False],),
         (ul.repeat, ('foo', 3), {}, ['foo', 'foo', 'foo'],),
 
-        (ul.from_seq, (range(3), "int"), {}, [0, 1, 2],),
         (ul.from_seq, (range(3), "float"), {}, [0.0, 1.0, 2.0],),
-        (ul.from_seq, ([False, True], "bool"), {}, [False, True],),
-        (ul.from_seq, ([0, 1, 2], "int"), {}, [0, 1, 2],),
-        (ul.from_seq, ((0, 1, 2), "int"), {}, [0, 1, 2],),
+        (ul.from_seq, (range(3), "int"), {}, [0, 1, 2],),
 
+        (ul.from_seq, ([False, True], "bool"), {}, [False, True],),
+        (ul.from_seq, ([0.0, 1.0, 2.0], "float"), {}, [0.0, 1.0, 2.0],),
+        (ul.from_seq, ([0, 1, 2], "int"), {}, [0, 1, 2],),
+        (ul.from_seq, (['foo', 'bar'], "string"), {}, ['foo', 'bar'],),
+
+        (ul.from_seq, ((False, True), "bool"), {}, [False, True],),
+        (ul.from_seq, ((0.0, 1.0, 2.0), "float"), {}, [0.0, 1.0, 2.0],),
+        (ul.from_seq, ((0, 1, 2), "int"), {}, [0, 1, 2],),
+        (ul.from_seq, (('foo', 'bar'), "string"), {}, ['foo', 'bar'],),
+
+        (ul.cycle, (range(3), 1, 'float'), {}, [0.0],),
         (ul.cycle, (range(3), 1, 'int'), {}, [0],),
         (ul.cycle, (range(3), 2, 'int'), {}, [0, 1],),
         (ul.cycle, (range(3), 3, 'int'), {}, [0, 1, 2],),
         (ul.cycle, (range(3), 4, 'int'), {}, [0, 1, 2, 0],),
         (ul.cycle, (range(3), 5, 'int'), {}, [0, 1, 2, 0, 1],),
         (ul.cycle, (range(3), 6, 'int'), {}, [0, 1, 2, 0, 1, 2],),
-        (ul.cycle, ([0.0, 1.0], 3, 'float'), {}, [0.0, 1.0, 0.0],),
-        (ul.cycle, ((True, False), 3, 'bool'), {}, [True, False, True],),
-        (ul.cycle, (('foo', 'bar'), 3, 'string'), {}, ['foo', 'bar', 'foo'],),
 
+        (ul.cycle, ([True, False], 3, 'bool'), {}, [True, False, True],),
+        (ul.cycle, ([0.0, 1.0], 3, 'float'), {}, [0.0, 1.0, 0.0],),
+        (ul.cycle, ([0, 1], 3, 'int'), {}, [0, 1, 0],),
+        (ul.cycle, (['foo', 'bar'], 3, 'string'), {}, ['foo', 'bar', 'foo'],),
+
+        (ul.cycle, ((True, False), 3, 'bool'), {}, [True, False, True],),
+        (ul.cycle, ((0.0, 1.0), 3, 'float'), {}, [0.0, 1.0, 0.0],),
+        (ul.cycle, ((0, 1), 3, 'int'), {}, [0, 1, 0],),
+        (ul.cycle, (('foo', 'bar'), 3, 'string'), {}, ['foo', 'bar', 'foo'],),
     ],
 )
 def test_constructors(
