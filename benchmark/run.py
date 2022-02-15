749f579452e20dc8b21b656cd72f89167278da95
tushushu <m18591915960@163.com>
2022-02-15 20:10:58 +0800

    benchmarking job

--- benchmark/run.py
+++ benchmark/run.py
@@ -1,6 +1,22 @@
 from integer import ArraySum
+import platform
+import ulist as ul
+import numpy as np
 
 if __name__ == "__main__":
     print("Benchmarking...")
+    print()
+
+    line = "=" * 32
+    print(line)
+    print("System OS:", platform.system())
+    print("Ulist version:", ul.__version__)
+    print("Numpy version:", np.__version__)
+    print(line)
+    print()
+
     result = ArraySum().run()
+    line = "-" * 128
+    print(line)
     print(result.name, result.dtype, result.scores)
+    print(line)
