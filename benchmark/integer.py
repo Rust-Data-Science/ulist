f923c068508620f81adcdb68e9137750d813d8cb
tushushu <m18591915960@163.com>
2022-02-19 20:06:24 +0800

    add cases

--- benchmark/integer.py
+++ benchmark/integer.py
@@ -40,6 +40,57 @@ class ArraySum(Benchmarker):
         args[0].sum()
 
 
+class EqualOne(Benchmarker):
+    def cases(self) -> list:
+        return [
+            ([randint(1, 10) for _ in range(100)],),
+            ([randint(1, 10) for _ in range(1000)],),
+            ([randint(1, 10) for _ in range(10000)],),
+            ([randint(1, 10) for _ in range(100000)],),
+            ([randint(1, 10) for _ in range(1000000)],),
+        ]
+
+    def ulist_fn(self, args) -> None:
+        args[0] == 1
+
+    def other_fn(self, args) -> None:
+        args[0] == 1
+
+
+class Max(Benchmarker):
+    def cases(self) -> list:
+        return [
+            ([x for x in range(100)],),
+            ([x for x in range(1000)],),
+            ([x for x in range(10000)],),
+            ([x for x in range(100000)],),
+            ([x for x in range(1000000)],),
+        ]
+
+    def ulist_fn(self, args) -> None:
+        args[0].max()
+
+    def other_fn(self, args) -> None:
+        args[0].max()
+
+
+class MulTwo(Benchmarker):
+    def cases(self) -> list:
+        return [
+            ([x for x in range(100)],),
+            ([x for x in range(1000)],),
+            ([x for x in range(10000)],),
+            ([x for x in range(100000)],),
+            ([x for x in range(1000000)],),
+        ]
+
+    def ulist_fn(self, args) -> None:
+        args[0] * 2
+
+    def other_fn(self, args) -> None:
+        args[0] * 2
+
+
 class UniqueElem(Benchmarker):
     def cases(self) -> list:
         return [
@@ -55,3 +106,20 @@ class UniqueElem(Benchmarker):
 
     def other_fn(self, args) -> None:
         np.unique(args[0])
+
+
+class Sort(Benchmarker):
+    def cases(self) -> list:
+        return [
+            ([randint(1, 100) for _ in range(100)],),
+            ([randint(1, 1000) for _ in range(1000)],),
+            ([randint(1, 10000) for _ in range(10000)],),
+            ([randint(1, 100000) for _ in range(100000)],),
+            ([randint(1, 1000000) for _ in range(1000000)],),
+        ]
+
+    def ulist_fn(self, args) -> None:
+        args[0].sort(ascending=True)
+
+    def other_fn(self, args) -> None:
+        args[0].sort()
