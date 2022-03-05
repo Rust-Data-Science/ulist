aa2a9aa9f9e4ad90d093b1726b75b52f7deb7811
tushushu <m18591915960@163.com>
2022-03-05 08:17:47 +0800

    add back method

--- ulist/src/index.rs
+++ ulist/src/index.rs
@@ -4,3 +4,17 @@ use pyo3::prelude::*;
 pub struct IndexList {
     _values: Vec<u32>,
 }
+
+#[pymethods]
+impl IndexList {
+    // Arrange the following methods in alphabetical order.
+
+    #[new]
+    pub fn new(vec: Vec<u32>) -> Self {
+        IndexList { _values: vec }
+    }
+
+    pub fn back(&self) -> u32 {
+        self._values[self._values.len()]
+    }
+}
