e3a61bd297a501aba354ad8f2dbb792a4a7fab79
tushushu <m18591915960@163.com>
2022-03-30 20:52:31 +0800

    typedef for FloatList64

--- ulist/python/ulist/typedef.py
+++ ulist/python/ulist/typedef.py
@@ -1,12 +1,18 @@
 from typing import Dict, List, Union
 
-from .ulist import (BooleanList, FloatList, IntegerList32, IntegerList64,
-                    StringList)
+from .ulist import (
+    BooleanList,
+    FloatList32,
+    FloatList64,
+    IntegerList32,
+    IntegerList64,
+    StringList
+)
 
 ELEM = Union[int, float, bool, str]
 NUM = Union[int, float]
 LIST_PY = Union[List[float], List[int], List[bool], List[str]]
-LIST_RS = Union[FloatList, IntegerList32,
+LIST_RS = Union[FloatList32, FloatList64, IntegerList32,
                 IntegerList64, BooleanList, StringList]
-NUM_LIST_RS = Union[FloatList, IntegerList32, IntegerList64]
+NUM_LIST_RS = Union[FloatList32, FloatList64, IntegerList32, IntegerList64]
 COUNTER = Union[Dict[int, int], Dict[bool, int], Dict[str, int]]
