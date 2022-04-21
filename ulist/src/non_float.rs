2db18f4921eed5a695fc53f39e8e52dcd1f38d54
tushushu <m18591915960@163.com>
2022-04-21 13:34:58 +0800

    make unique faster

--- ulist/src/non_float.rs
+++ ulist/src/non_float.rs
@@ -2,7 +2,6 @@ use crate::base::List;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::hash::Hash;
-use std::iter::FromIterator;
 
 pub trait NonFloatList<T>: List<T>
 where
@@ -59,17 +58,30 @@ where
         }
     }
 
-    fn unique(&self) -> Self {
+    unsafe fn unique(&self) -> Self {
+        // Get the unique values.
         let dedup = HashSet::with_capacity(self.size());
-        for (i, &elem) in self.values().iter().enumerate() {
+        for (i, &val) in self.values().iter().enumerate() {
             if self.na_indexes().contains(&i) {
                 continue;
             }
-            dedup.insert(elem);
+            dedup.insert(val);
         }
-        //TODO: Capacity of vec?
-        let vec = Vec::from_iter(dedup);
-        vec.push(self.na_value());
+        // Copy the unique and na values to the vec.
+        let mut l = dedup.len();
+        if self.count_na() > 0 {
+            l += 1;
+        }
+        let vec = Vec::with_capacity(l);
+        vec.set_len(l);
+        for (i, &val) in dedup.iter().enumerate() {
+            let elem = vec.get_unchecked_mut(i);
+            *elem = val;
+        }
+        if self.count_na() > 0 {
+            vec.push(self.na_value());
+        }
+        // Construct List.
         let hset = HashSet::new();
         hset.insert(vec.len() - 1);
         List::_new(vec, hset)
