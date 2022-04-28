b898c1f347854fb82b6420c452664a0372d4e473
tushushu <m18591915960@163.com>
2022-04-28 21:52:41 +0800

    bool stub

--- ulist/python/ulist/typedef.py
+++ ulist/python/ulist/typedef.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
 
 from .ulist import (
     BooleanList,
@@ -10,6 +10,7 @@ from .ulist import (
 )
 
 ELEM = Union[int, float, bool, str]
+ELEM_OPT = Union[Optional[int], Optional[float], Optional[bool], Optional[str]]
 NUM = Union[int, float]
 LIST_PY = Union[List[float], List[int], List[bool], List[str]]
 LIST_RS = Union[FloatList32, FloatList64, IntegerList32,
