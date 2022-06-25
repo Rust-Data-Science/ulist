d0ad813c1fb455065ec61703224020b58ba2b189
yingmanwumen <yingmanwumen@foxmail.com>
2022-06-25 23:29:12 +0800

    move check_version() into a separate file

--- tests/run.sh
+++ tests/run.sh
@@ -1,23 +1,6 @@
-EXIT_STATUS=0
+. ./check_version.sh
 
-# check the ulist versions first
-check_version() {
-        local pp="../ulist/python/ulist/__init__.py"
-        local pd="../docs/conf.py"
-        local pc="../ulist/Cargo.toml"
-        local vp=$(grep "__version" $pp | awk '{print $NF}' | sed 's/"//g')
-        local vd=$(grep "release = " $pd | awk '{print $NF}' | sed "s/'//g")
-        local vc=$(grep "version = " $pc | head -1 | awk '{print $NF}' | sed 's/"//g')
-        if [ $vp != $vd -o $vp != $vc ]; then
-                echo "Ulist versions don't match!"
-                echo "$pp: $vp"
-                echo "$pd: $vd"
-                echo "$pc: $vc"
-                echo "\n"
-                return 1
-        fi
-        return 0
-}
+EXIT_STATUS=0
 
 check_version || EXIT_STATUS=$?
 
